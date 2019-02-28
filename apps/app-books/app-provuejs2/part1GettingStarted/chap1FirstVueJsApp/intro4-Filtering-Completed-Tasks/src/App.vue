04f8873efe07632a376367a1449de8da3d933102
Teoman Shipahi <aytekoz@yahoo.com>
2019-02-27 22:59:16 -0500

    todo application - local storage

---
+++ apps/app-books/app-provuejs2/part1GettingStarted/chap1FirstVueJsApp/intro4-Filtering-Completed-Tasks/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">{{name}}'s To Do List</h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input">
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input">
+          <label class="form-check-label font-weight-bold">Hide completed tasks</label>
+        </div>
+      </div>
+      <div>
+        <h3>References</h3>
+        <ul>
+          <li>
+            <a v-bind:href="referenceLink">{{referenceLink}}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      name: "Adam",
+      referenceLink:
+        "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter",
+      tasks: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false }
+      ],
+      hideCompleted: true
+    };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;
+    }
+  }
+};
+</script>
---
+++ apps/app-books/app-provuejs2/part1GettingStarted/chap1FirstVueJsApp/intro6-Storing-Data-Persistently/README-ggg.md
@@ -0,0 +1,12 @@
+### Installing the @vue/cli Package
+
+The @vue/cli package is the standard way to create and manage Vue.js projects during development. You
+don’t have to use this package, but it provides everything that is needed to get started with Vue.js, and I use it
+throughout this book.
+
+### Installing the Vue Tools Package
+
+```
+$ npm i
+$ npm run serve
+```
