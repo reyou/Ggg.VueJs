06fb0cbdf5edf8184955eeb6d1271d6b5dd80e1b
Teoman Shipahi <aytekoz@yahoo.com>
2019-03-02 13:26:04 -0500

    Essential JavaScript Primer

--- apps/app-books/app-provuejs2/part1GettingStarted/chap3-Html-and-Css-Primer/intro1/htmlcssprimer/src/App.vue
+++ apps/app-books/app-provuejs2/part1GettingStarted/chap3-Html-and-Css-Primer/intro1/htmlcssprimer/src/App.vue
@@ -1,28 +1,92 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- body container 
+  https://getbootstrap.com/docs/4.0/layout/overview/
+  https://getbootstrap.com/docs/4.0/layout/grid/-->
+  <div class="container-fluid">
+    <div class="row">
+      <!-- left container 
+      https://getbootstrap.com/docs/4.0/utilities/display/-->
+      <div class="col-2 d-none d-sm-block" style>
+        <!-- vertical navbar 
+        https://getbootstrap.com/docs/4.0/components/navs/-->
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+      <!-- right container -->
+      <div class="col" style>
+        <h4 class="bg-primary text-white text-center p-2">Adam's To Do List</h4>
+        <!-- grid -->
+        <div class="container-fluid p-4">
+          <!-- headers -->
+          <div class="row">
+            <div class="col font-weight-bold">Task</div>
+            <div class="col-2 font-weight-bold">Done</div>
+          </div>
+          <!-- rows -->
+          <div class="row" v-for="t in tasks" v-bind:key="t.action">
+            <div class="col">{{t.action}}</div>
+            <div class="col-2">{{t.done}}</div>
+          </div>
+        </div>
+        <!-- table  -->
+        <h1>Table View</h1>
+        <table class="table table-striped table-bordered table-sm">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="t in tasks" v-bind:key="t.action">
+              <td>{{t.action}}</td>
+              <td>{{t.done}}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="form-group m-2">
+          <label>New Item:</label>
+          <input v-model="newItemText" class="form-control">
+        </div>
+        <div class="text-center">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      tasks: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false }
+      ],
+      newItemText: ""
+    };
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      });
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
