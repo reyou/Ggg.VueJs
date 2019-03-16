755f55367767439becb2ad913f31c4807a6022c9
Teoman Shipahi <aytekoz@yahoo.com>
2019-03-15 23:40:02 -0400

    js premiere

---
+++ apps/app-books/app-provuejs2/part1GettingStarted/chap4-Essential-Js-Primer/intro1/jsprimer/src/main.3.js
@@ -0,0 +1,8 @@
+function myFunc(name, weather, ...extraArgs) {
+  console.log("Hello" + name + ".");
+  console.log("It is " + weather + " today.");
+  for (let i = 0; i < extraArgs.length; i++) {
+    console.log("Extra Arg:" + extraArgs[i]);
+  }
+}
+myFunc("Adam", "sunny", "one", "two", "three");
