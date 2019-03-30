161e2aa0c3902b6737f1b45ebd8c02c4aa798675
Teoman Shipahi <aytekoz@yahoo.com>
2019-03-29 22:39:10 -0400

    104

--- apps/app-books/app-provuejs2/part1GettingStarted/chap5-Sports-Store-A-Real-Application/intro1/sportsstore/src/main.js
+++ apps/app-books/app-provuejs2/part1GettingStarted/chap5-Sports-Store-A-Real-Application/intro1/sportsstore/src/main.js
@@ -5,7 +5,9 @@ Vue.config.productionTip = false;
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
+import store from "./store";
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount("#app");
